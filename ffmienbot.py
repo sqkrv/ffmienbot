8f818de6ddd8c30c33b9369494c52dfb0f574c30
sqkrv <kochkarev02@gmail.com>
2023-03-07 16:02:28 +0300

    fixed help command super finally

--- ffmienbot.py
+++ ffmienbot.py
@@ -86,8 +86,8 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
         "• /help — показать данной сообщение"
-        "\n• /suggest_post — предложить пост в КРУГИ НА ФИЗМАТЕ"
-        "\n• /suggest_gossip — предложить пост в [физматовские сплетни](https://t\.me/spletniffmien)"
+        "\n• /suggest\_post — предложить пост в КРУГИ НА ФИЗМАТЕ"
+        "\n• /suggest\_gossip — предложить пост в [физматовские сплетни](https://t\.me/spletniffmien)"
         "\n\n||Данный бот является полностью неофициальным и никак"
         "не связан с РУДН и его руководством\."
         "\nПо всем претензиям, вопросам и предложениям обращайтесь к @sqkrv||",  # todo feedback channel
